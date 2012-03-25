0302bf3f4beb6e4b3fcc1a75a22f95a9f40ca164
Dan Bowen <dbowen1@mac.com>
2012-03-25 19:43:26 -0400

    Fixed logic on checking EEPROM adds dupes

--- Source/CommCtrlrArduino/I2CCommMgr.cpp
+++ Source/CommCtrlrArduino/I2CCommMgr.cpp
@@ -217,20 +217,16 @@ void I2CCommMgr::I2CParse(I2CMsg i2cMsg)
       //Check for duplicate start and end addresses
       if( (i2cMsg.i2cData[0] == i2cMsg.i2cData[2]) && (i2cMsg.i2cData[1] == i2cMsg.i2cData[3])) {
 	  	DebugMsg::msg_P("I2C",'W',PSTR("LongMsg Start and End Addrs The Same.  No Send."));
-	  	break;
-	  }
-	  
-      LongMsg msg(i2cMsg.i2cData[0],i2cMsg.i2cData[1],i2cMsg.i2cData[2],i2cMsg.i2cData[3]);
-     
-      //DebugMsg::msg_P("I2C",'I',PSTR("Store Long Message"));
-	  
-
-	  
-	  if (SatQueue::getInstance().write(msg))
-	  {
-	  	//DebugMsg::msg_P("I2C",'I',PSTR("Report Stored OK."));
 	  } else {
-	  	DebugMsg::msg_P("I2C",'W',PSTR("Report Store FAILED."));
+		  DebugMsg::msg_P("I2C",'I',PSTR("Store Long Message"));
+		  LongMsg msg(i2cMsg.i2cData[0],i2cMsg.i2cData[1],i2cMsg.i2cData[2],i2cMsg.i2cData[3]);
+	
+		  if (SatQueue::getInstance().write(msg))
+		  {
+			//DebugMsg::msg_P("I2C",'I',PSTR("Report Stored OK."));
+		  } else {
+			DebugMsg::msg_P("I2C",'W',PSTR("Report Store FAILED."));
+		  }
 	  }
       break;
     }
