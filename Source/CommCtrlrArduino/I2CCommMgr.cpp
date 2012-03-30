1a49468ea2e5a8e410f4002f09afa227a58271cd
Dan Bowen <dbowen1@mac.com>
2012-03-30 16:00:59 -0400

    autoformat in arduino

--- Source/CommCtrlrArduino/I2CCommMgr.cpp
+++ Source/CommCtrlrArduino/I2CCommMgr.cpp
@@ -15,7 +15,8 @@
  Last Revised December 13, 2010
  */
 
-I2CCommMgr::I2CCommMgr(SatCommMgr& satCommMgr):_satCommMgr(satCommMgr)
+I2CCommMgr::I2CCommMgr(SatCommMgr& satCommMgr):
+_satCommMgr(satCommMgr)
 {
 }
 
@@ -23,64 +24,64 @@ I2CCommMgr::I2CCommMgr(SatCommMgr& satCommMgr):_satCommMgr(satCommMgr)
 int I2CCommMgr::I2CXmit(byte device, byte command, byte* data, int length)
 {
 
-	int sentStatus;
-	  // Transmit over I2C
-	for (unsigned int i = 1; i < i2cRetryLimit; i++) 
-	{
-		  Wire.beginTransmission(device);                      // Begin Transmission to (address)
-		#if (ARDUINO >= 100)
-		  Wire.write(command);                                   // Put (command) on queue
-		#else
-		  Wire.send(command);                                   // Put (command) on queue
-		#endif
-		  for (int i = 0;i < length; i++)                       // Loop to put all (data) on queue
-		  {
-		#if (ARDUINO >= 100)
-			Wire.write(data[i]);                                   // Put (command) on queue
-		#else
-			Wire.send(data[i]);                                   // Put (command) on queue
-		#endif
-		  }
-		  sentStatus = Wire.endTransmission();              // Send queue, will return status
-		  if (sentStatus == 0) 
-		  {
-		  break;
-		  }
-		  Serial.println(F("I2CTXERR"));
-		  Serial.flush();
-		  //If it didn't' suceed for  any reason, try again
-		  delay(random(((i-1)*(i-1)*100),(i*i*100)));  //Delay for a random time between (i-1)^2*100 millis i^2*100 millis
-	}
-    return sentStatus;                                   // Return Status of Transmission	
+  int sentStatus;
+  // Transmit over I2C
+  for (unsigned int i = 1; i < i2cRetryLimit; i++) 
+  {
+    Wire.beginTransmission(device);                      // Begin Transmission to (address)
+#if (ARDUINO >= 100)
+    Wire.write(command);                                   // Put (command) on queue
+#else
+    Wire.send(command);                                   // Put (command) on queue
+#endif
+    for (int i = 0;i < length; i++)                       // Loop to put all (data) on queue
+    {
+#if (ARDUINO >= 100)
+      Wire.write(data[i]);                                   // Put (command) on queue
+#else
+      Wire.send(data[i]);                                   // Put (command) on queue
+#endif
+    }
+    sentStatus = Wire.endTransmission();              // Send queue, will return status
+    if (sentStatus == 0) 
+    {
+      break;
+    }
+    Serial.println(F("I2CTXERR"));
+    Serial.flush();
+    //If it didn't' suceed for  any reason, try again
+    delay(random(((i-1)*(i-1)*100),(i*i*100)));  //Delay for a random time between (i-1)^2*100 millis i^2*100 millis
+  }
+  return sentStatus;                                   // Return Status of Transmission	
 }
 
 int I2CCommMgr::I2CXmitMsg(byte device, byte* data, int length)
 {
 
-	int sentStatus;
-	  // Transmit over I2C
-	for (unsigned int i = 1; i < i2cRetryLimit; i++) 
-	{
-	  // Transmit over I2C
-	  Wire.beginTransmission(device);                      // Begin Transmission to (address)
-	  for (int i = 0;i < length; i++)                       // Loop to put all (data) on queue
-	  {
-	#if (ARDUINO >= 100)
-		Wire.write(data[i]);                                   // Put (command) on queue
-	#else
-		Wire.send(data[i]);                                   // Put (command) on queue
-	#endif
-	  }
-	  sentStatus = Wire.endTransmission();              // Send queue, will return status
-	  if (sentStatus == 0) 
-		  {
-		  break;
-		  }
-		  Serial.println(F("I2CTXERR"));
-		  Serial.flush();
-		  //If it didn't' suceed for  any reason, try again
-		  delay(random(((i-1)*(i-1)*100),(i*i*100)));  //Delay for a random time between (i-1)^2*100 millis i^2*100 millis
-	}
+  int sentStatus;
+  // Transmit over I2C
+  for (unsigned int i = 1; i < i2cRetryLimit; i++) 
+  {
+    // Transmit over I2C
+    Wire.beginTransmission(device);                      // Begin Transmission to (address)
+    for (int i = 0;i < length; i++)                       // Loop to put all (data) on queue
+    {
+#if (ARDUINO >= 100)
+      Wire.write(data[i]);                                   // Put (command) on queue
+#else
+      Wire.send(data[i]);                                   // Put (command) on queue
+#endif
+    }
+    sentStatus = Wire.endTransmission();              // Send queue, will return status
+    if (sentStatus == 0) 
+    {
+      break;
+    }
+    Serial.println(F("I2CTXERR"));
+    Serial.flush();
+    //If it didn't' suceed for  any reason, try again
+    delay(random(((i-1)*(i-1)*100),(i*i*100)));  //Delay for a random time between (i-1)^2*100 millis i^2*100 millis
+  }
   return sentStatus;                                   // Return Status of Transmission
 }
 
@@ -99,12 +100,12 @@ void I2Csend(byte length) {
 void I2CCommMgr::i2cInit()
 {
   Wire.begin(i2cCommCtrlAddr);                                   // Join I2C Bus as slave
-   #define TWI_FREQ_WSBFAST 400000UL
-   #ifndef CPU_FREQ
-   	#define CPU_FREQ = 16000000UL
-   #endif
-	TWBR = ((CPU_FREQ / TWI_FREQ_WSBFAST) - 16) / 2;  // Make I2C FASTER! to 400KHz
-   
+#define TWI_FREQ_WSBFAST 400000UL
+#ifndef CPU_FREQ
+#define CPU_FREQ = 16000000UL
+#endif
+  TWBR = ((CPU_FREQ / TWI_FREQ_WSBFAST) - 16) / 2;  // Make I2C FASTER! to 400KHz
+
   Wire.onReceive(I2CCommMgr::i2cReceiveData);                            // Set On Receive Handler
   DebugMsg::msg_P("I2C",'I',PSTR("I2C Init Done. Addr %x"),i2cCommCtrlAddr);
 
@@ -121,8 +122,8 @@ void I2CCommMgr::i2cReceiveData(int wireDataLen)
 
   // Check to see if the I2C messsage is too big to handle if so throw it away
   if ((wireDataLen -1) > i2cMaxDataLen) {
-	Serial.println("2B");
-	DebugMsg::msg_P("I2C",'I',PSTR("\ni2cRx: data too big!"));
+    Serial.println("2B");
+    DebugMsg::msg_P("I2C",'I',PSTR("\ni2cRx: data too big!"));
     while(Wire.available() > 0)           // Loop to receive the Data from the I2C Bus
     {
 #if (ARDUINO >= 100)
@@ -136,9 +137,9 @@ void I2CCommMgr::i2cReceiveData(int wireDataLen)
   {
     i2cMsg.i2cDataLen = wireDataLen - 1;
 #if (ARDUINO >= 100)
-      i2cMsg.i2cRxCommand = Wire.read();          // Receive the Command as the first byte from the I2C Bus
+    i2cMsg.i2cRxCommand = Wire.read();          // Receive the Command as the first byte from the I2C Bus
 #else
-      i2cMsg.i2cRxCommand = Wire.receive();          // Receive the Command as the first byte from the I2C Bus
+    i2cMsg.i2cRxCommand = Wire.receive();          // Receive the Command as the first byte from the I2C Bus
 #endif
 
     while(Wire.available() > 0)           // Loop to receive the Data from the I2C Bus
@@ -151,16 +152,17 @@ void I2CCommMgr::i2cReceiveData(int wireDataLen)
       i++;
     }
   }
-//  Serial.print("Cmd:");Serial.print(i2cMsg.i2cRxCommand);
+  //  Serial.print("Cmd:");Serial.print(i2cMsg.i2cRxCommand);
 #ifdef i2cDebug_DONT_ENABLE  // This makes the serial out too long and causes lockup
   // Printing code for debug usage                                                 
   DebugMsg::msg("I2C",'I',"i2c Packet Rx'd. Cmd: %x Data: ",i2cMsg.i2cRxCommand);
   for (int i = 0;i < i2cMsg.i2cDataLen-1; i++)
   {
-    Serial.print(i2cMsg.i2cData[i],HEX); Serial.print(" ")
-    //DebugMsg::msg("I2C",'I'," %0x",i2cMsg.i2cData[i]);
-  }
-  Serial.println();
+    Serial.print(i2cMsg.i2cData[i],HEX); 
+    Serial.print(" ")
+      //DebugMsg::msg("I2C",'I'," %0x",i2cMsg.i2cData[i]);
+    }
+    Serial.println();
 #endif
   // Store the Command
   I2CQueue::getInstance().write(i2cMsg);
@@ -169,15 +171,15 @@ void I2CCommMgr::i2cReceiveData(int wireDataLen)
 
 /*
   Look at the queue and see if there is anything that needs to be processed.
-*/
+ */
 void I2CCommMgr::update()
 {
-    
-    if (  I2CQueue::getInstance().count() > 0)  // Got a message that needs processing
-    {
-      I2CParse( I2CQueue::getInstance().read());
-      //Serial.print("vvvvvvv Q:"); Serial.println(I2CQueue::getInstance().count());
-    }
+
+  if (  I2CQueue::getInstance().count() > 0)  // Got a message that needs processing
+  {
+    I2CParse( I2CQueue::getInstance().read());
+    //Serial.print("vvvvvvv Q:"); Serial.println(I2CQueue::getInstance().count());
+  }
 }
 
 
@@ -190,85 +192,88 @@ void I2CCommMgr::I2CParse(I2CMsg i2cMsg)
   Serial.flush();
   for (int i = 0;i < i2cMsg.i2cDataLen; i++)
   {
-    Serial.print(i2cMsg.i2cData[i],HEX); Serial.print(" ");
+    Serial.print(i2cMsg.i2cData[i],HEX); 
+    Serial.print(" ");
   }
   Serial.println();
   Serial.flush();
 #endif
   switch(i2cMsg.i2cRxCommand){
 
-    case i2cCmdSATTXATCRpt: 
+  case i2cCmdSATTXATCRpt: 
     { 
       DebugMsg::msg_P("I2C",'I',PSTR("Store ATC Rpt"));
-/*
+      /*
 #if 0
-      //Store Latest position and Epoch Time for later use
-      latestPosition[0]=i2cdata[i2cSel][6];  //lat0
-      latestPosition[1]=i2cdata[i2cSel][7];  //lat1
-      latestPosition[2]=i2cdata[i2cSel][8];  //lat2
-      latestPosition[3]=i2cdata[i2cSel][0];  //lon0
-      latestPosition[4]=i2cdata[i2cSel][1];  //lon1
-      latestPosition[5]=i2cdata[i2cSel][2];  //lon2
-      satSaveATCPkt(); //save the ATC packet to be ready to send when sat is ready to accept.
-      unsigned int epochMinutes = word((i2cMsg.i2cData[4] & B00001111),i2cMsg.i2cData[5]);  //Copy epochMinute value into ram
-      //           Lat0               Lat1               Lat2               Lon0               Lon1               Lon2               Alt
-      ShortMsg msg(i2cMsg.i2cData[6], i2cMsg.i2cData[7], i2cMsg.i2cData[8], i2cMsg.i2cData[0], i2cMsg.i2cData[1], i2cMsg.i2cData[2], 0 , epochMinutes);
-	  SatQueue::getInstance().write(&msg);
-#endif
-*/
+       //Store Latest position and Epoch Time for later use
+       latestPosition[0]=i2cdata[i2cSel][6];  //lat0
+       latestPosition[1]=i2cdata[i2cSel][7];  //lat1
+       latestPosition[2]=i2cdata[i2cSel][8];  //lat2
+       latestPosition[3]=i2cdata[i2cSel][0];  //lon0
+       latestPosition[4]=i2cdata[i2cSel][1];  //lon1
+       latestPosition[5]=i2cdata[i2cSel][2];  //lon2
+       satSaveATCPkt(); //save the ATC packet to be ready to send when sat is ready to accept.
+       unsigned int epochMinutes = word((i2cMsg.i2cData[4] & B00001111),i2cMsg.i2cData[5]);  //Copy epochMinute value into ram
+       //           Lat0               Lat1               Lat2               Lon0               Lon1               Lon2               Alt
+       ShortMsg msg(i2cMsg.i2cData[6], i2cMsg.i2cData[7], i2cMsg.i2cData[8], i2cMsg.i2cData[0], i2cMsg.i2cData[1], i2cMsg.i2cData[2], 0 , epochMinutes);
+       	  SatQueue::getInstance().write(&msg);
+       #endif
+       */
       break;
     } 
-    
-    case i2cCmdSATTxFrmEEPROM: 
+
+  case i2cCmdSATTxFrmEEPROM: 
     { 
       //Check for duplicate start and end addresses
       if( (i2cMsg.i2cData[0] == i2cMsg.i2cData[2]) && (i2cMsg.i2cData[1] == i2cMsg.i2cData[3])) {
-	  	DebugMsg::msg_P("I2C",'W',PSTR("LongMsg Start and End Addrs The Same.  No Send."));
-	  } else {
-		  DebugMsg::msg_P("I2C",'I',PSTR("Store Long Message"));
-		  LongMsg msg(i2cMsg.i2cData[0],i2cMsg.i2cData[1],i2cMsg.i2cData[2],i2cMsg.i2cData[3]);
-	
-		  if (SatQueue::getInstance().write(msg))
-		  {
-			//DebugMsg::msg_P("I2C",'I',PSTR("Report Stored OK."));
-		  } else {
-			DebugMsg::msg_P("I2C",'W',PSTR("Report Store FAILED."));
-		  }
-	  }
+        DebugMsg::msg_P("I2C",'W',PSTR("LongMsg Start and End Addrs The Same.  No Send."));
+      } 
+      else {
+        DebugMsg::msg_P("I2C",'I',PSTR("Store Long Message"));
+        LongMsg msg(i2cMsg.i2cData[0],i2cMsg.i2cData[1],i2cMsg.i2cData[2],i2cMsg.i2cData[3]);
+
+        if (SatQueue::getInstance().write(msg))
+        {
+          //DebugMsg::msg_P("I2C",'I',PSTR("Report Stored OK."));
+        } 
+        else {
+          DebugMsg::msg_P("I2C",'W',PSTR("Report Store FAILED."));
+        }
+      }
       break;
     }
-    
-  
-    case i2cCmdCDNHeartBeat: 
+
+
+  case i2cCmdCDNHeartBeat: 
     { 
       DebugMsg::msg_P("I2C",'I',PSTR("CutDn Heart Beat"));
       CutDown::ResetTimer();
       break;
     }
-    
-    case i2cCmdCDNSetTimerAndReset: 
+
+  case i2cCmdCDNSetTimerAndReset: 
     { 
       DebugMsg::msg_P("I2C",'I',PSTR("CutDn Set Timer and Reset"));
       CutDown::CmdSet(i2cMsg.i2cData[0]);  //Take 1 byte 0-255 for minutes
       break;
     }
 
-    
-    case i2cCmdCDNCUTDOWNNOW: 
+
+  case i2cCmdCDNCUTDOWNNOW: 
     { 
       DebugMsg::msg_P("I2C",'I',PSTR("CutDn Now"));
       CutDown::CutdownNOW();
       break;
     }
-    
-    case i2cCmdSATPowerOn: 
+
+  case i2cCmdSATPowerOn: 
     { 
       DebugMsg::msg_P("I2C",'I',PSTR("SatModem ON"));
-	  _satCommMgr.turnModemOn();
+      _satCommMgr.turnModemOn();
       break;
     }
-    
-    case i2cCmdSATPowerOff: 
+
+  case i2cCmdSATPowerOff: 
     {
       DebugMsg::msg_P("I2C",'I',PSTR("SatModem OFF"));
       _satCommMgr.turnModemOff();
@@ -281,6 +286,7 @@ void I2CCommMgr::I2CParse(I2CMsg i2cMsg)
     }
   }
 }
+
 
 
 
