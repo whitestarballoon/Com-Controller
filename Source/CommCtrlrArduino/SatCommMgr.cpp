fb231fbca172df21eaf97a1b0b92c590904b3d76
Dan Bowen <dbowen1@mac.com>
2012-04-22 01:25:27 -0400

    Adding code to send commands to I2C

--- Source/CommCtrlrArduino/SatCommMgr.cpp
+++ Source/CommCtrlrArduino/SatCommMgr.cpp
@@ -256,7 +256,7 @@ void SatCommMgr::parseIncommingMsg(unsigned char* packetBufferLocal,unsigned int
 			packetBufferA[i-packetPayloadStartIndex+3]=packetBufferLocal[i];  
 		}
 		
-		 int i2cretval = (*_i2cCommMgr).I2CXmit(packetBufferLocal[packetPayloadStartIndex + 1], packetBufferLocal[packetPayloadStartIndex + 2], packetBufferA, packLen-satIncomingMessageHeaderLength);   //Send to I2C
+		 int i2cretval = (*_i2cCommMgr).I2CXmit(packetBufferLocal[packetPayloadStartIndex + 1], packetBufferLocal[packetPayloadStartIndex + 2], packetBufferA, packLen-satIncomingMessageHeaderLength-1);   //Send to I2C
 		//?May want to check for I2C success status here!
 		Serial.print(" I2C returned: ");
 		Serial.println(i2cretval);
